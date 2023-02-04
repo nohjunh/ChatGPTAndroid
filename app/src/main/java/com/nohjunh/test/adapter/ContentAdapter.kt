22c26ceb8a17b6658858b34afc9200d9b29d05f7
nohjunh <nsh042777@naver.com>
2023-02-04 15:42:25 +0900

    Add contentEntity RV Adapter setting

--- app/src/main/java/com/nohjunh/test/adapter/ContentAdapter.kt
+++ app/src/main/java/com/nohjunh/test/adapter/ContentAdapter.kt
@@ -1,4 +1,49 @@
 package com.nohjunh.test.adapter
 
-class ContentAdapter() {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.nohjunh.test.R
+import com.nohjunh.test.database.entity.ContentEntity
+
+class ContentAdapter(val context : Context, private val dataSet : List<ContentEntity>) : RecyclerView.Adapter<ContentAdapter.ViewHolder>() {
+
+    interface DelBtnClick {
+        fun onClick(view : View, position: Int)
+    }
+    var delBtnClick : DelBtnClick? = null
+
+    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        val contentTV : TextView = view.findViewById(R.id.rvItemTV)
+        val delBtn : Button = view.findViewById(R.id.delBtn)
+        val idHolder : TextView = view.findViewById(R.id.holdingId)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.content_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.contentTV.text = dataSet[position].content
+        holder.idHolder.text = dataSet[position].id.toString()
+
+        holder.delBtn.setOnClickListener { view ->
+            delBtnClick?.onClick(view, position)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
 }
--- app/src/main/java/com/nohjunh/test/view/MainActivity.kt
+++ app/src/main/java/com/nohjunh/test/view/MainActivity.kt
@@ -17,5 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
     }
 }
