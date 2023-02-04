a6758727b5ee991fcf83538667574577460869fc
nohjunh <nsh042777@naver.com>
2023-02-04 15:57:59 +0900

    Add insert Content logic

--- app/src/main/java/com/nohjunh/test/view/MainActivity.kt
+++ app/src/main/java/com/nohjunh/test/view/MainActivity.kt
@@ -3,21 +3,49 @@ package com.nohjunh.test.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nohjunh.test.adapter.ContentAdapter
+import com.nohjunh.test.database.entity.ContentEntity
 import com.nohjunh.test.databinding.ActivityMainBinding
 import com.nohjunh.test.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-
     private val viewModel : MainViewModel by viewModels()
+    private var contentDataList = ArrayList<ContentEntity>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 로딩 되었을 때 바로 content가 보이도록
+        viewModel.getContentData()
 
+        viewModel.contentList.observe(this, Observer {
+            contentDataList.clear()
+            for (entity in it) {
+                contentDataList.add(entity)
+            }
+            setContentListRV()
+        })
+
+        binding.sendBtn.setOnClickListener {
+            viewModel.insertContent(binding.EDView.text.toString())
+            binding.EDView.setText("")
+            viewModel.getContentData()
+        }
+
+    }
+
+    private fun setContentListRV() {
+        val contentAdapter = ContentAdapter(this, contentDataList)
+        binding.RVContainer.adapter = contentAdapter
+        binding.RVContainer.layoutManager = LinearLayoutManager(this)
+        // 맨 밑부터 보이게
+        binding.RVContainer.scrollToPosition(contentDataList.size-1)
 
     }
 }
--- app/src/main/java/com/nohjunh/test/viewModel/MainViewModel.kt
+++ app/src/main/java/com/nohjunh/test/viewModel/MainViewModel.kt
@@ -1,24 +1,32 @@
 package com.nohjunh.test.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nohjunh.test.database.entity.ContentEntity
 import com.nohjunh.test.repository.DatabaseRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     private val databaseRepository = DatabaseRepository()
 
-    fun getContentData() = viewModelScope.launch {
+    private var _contentList = MutableLiveData<List<ContentEntity>>()
+    val contentList : LiveData<List<ContentEntity>>
+        get() = _contentList
 
+    fun getContentData() = viewModelScope.launch(Dispatchers.IO) {
+        _contentList.postValue(databaseRepository.getContentData())
     }
 
-    fun insertContent(content : String) = viewModelScope.launch {
-
+    fun insertContent(content : String) = viewModelScope.launch(Dispatchers.IO) {
+        databaseRepository.insertContent(content)
     }
 
-    fun deleteSelectedContent(id : Int) = viewModelScope.launch {
-
+    fun deleteSelectedContent(id : Int) = viewModelScope.launch(Dispatchers.IO) {
+        databaseRepository.deleteSelectedContent(id)
     }
 
 }
